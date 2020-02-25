417439902b1799a88bd63dd58ebab04fa40ca815
Sydney Anne Erickson <sydneyerickson@icloud.com>
2020-02-24 18:19:52 -0800

    Add DYLD and XNU projects to grab headers from

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
+# Building libtapi is required for LD64 to build
 git clone https://github.com/tpoechtrager/apple-libtapi.git
 cd apple-libtapi
 INSTALLPREFIX=$HOME/Gardenia ./build.sh
 ./install.sh
+mkdir $HOME/Gardenia/include/llvm-c
+cp src/llvm/include/llvm-c/lto.h $HOME/Gardenia/include/llvm-c
+cd ..
+
+git clone https://github.com/ProjectGardenia/dyld.git
+cd dyld
+cp -r include/* $HOME/Gardenia/include/
+cd ..
+
+git clone https://github.com/ProjectGardenia/xnu.git
+cd xnu
+cp EXTERNAL_HEADERS/Availability.h $HOME/Gardenia/include/
+cp EXTERNAL_HEADERS/AvailabilityInternal.h $HOME/Gardenia/include/
 cd ..
 
 git clone https://github.com/ProjectGardenia/ld64.git
